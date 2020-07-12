b67da71079b1bcc8de99ff708d0d755ab86b06fe
revatiathavale <57042399+revatiathavale@users.noreply.github.com>
2020-07-12 15:54:55 -0400

    Update process.cpp

--- process.cpp
+++ process.cpp
@@ -31,6 +31,6 @@ long int Process::UpTime() { return LinuxParser::UpTime(Process::Pid()); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const {
+bool Process::operator<(Process const& a) const {
   return a.CpuUtilization() < CpuUtilization();
 }
