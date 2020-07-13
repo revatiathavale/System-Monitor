adcf0546ddf4d16ab9bac2320f9a5d3bd2b9d0b7
revatiathavale <57042399+revatiathavale@users.noreply.github.com>
2020-07-13 11:16:45 -0400

    Update process.cpp

--- process.cpp
+++ process.cpp
@@ -12,25 +12,25 @@ using std::to_string;
 using std::vector;
 
 // TODO: Return this process's ID
-int Process::Pid() { return pid; }
+int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return LinuxParser::CpuUtilization(); }
+float Process::CpuUtilization() const { return LinuxParser::CpuUtilization(); }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return LinuxParser::Command(Process::Pid()); }
+string Process::Command() { return LinuxParser::Command(pid_); }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return LinuxParser::Ram(Process::Pid()); }
+string Process::Ram() { return LinuxParser::Ram(pid_); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return LinuxParser::User(Process::Pid()); }
+string Process::User() { return LinuxParser::User(pid_); }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return LinuxParser::UpTime(Process::Pid()); }
+long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a) const {
-  return a.CpuUtilization() < CpuUtilization();
+  return a.CpuUtilization() < this->CpuUtilization();
 }
