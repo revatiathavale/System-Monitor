39f9929337ca3f6f75f7260d193f777d6f31cc32
revatiathavale <57042399+revatiathavale@users.noreply.github.com>
2020-07-18 18:29:40 -0400

    Update processor.cpp

--- processor.cpp
+++ processor.cpp
@@ -1,5 +1,5 @@
 #include "processor.h"
-#include "linux_parser.cpp"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { return LinuxParser::CpuUtilization(); }
